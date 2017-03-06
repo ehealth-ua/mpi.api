d3f8d4bcf31957def67c9fdff4178fe99dbb9817
gmile <iamexile@gmail.com>
2017-03-06 14:34:23 +0200

    Add persons table/schema

---
+++ lib/mpi/person.ex
@@ -0,0 +1,57 @@
+defmodule Mpi.Person do
+  use Ecto.Schema
+
+  import Ecto.Changeset
+
+  @primary_key {:id, Ecto.UUID, autogenerate: true}
+  schema "persons" do
+    field :first_name, :string
+    field :last_name, :string
+    field :second_name, :string
+    field :birth_date, :date
+    field :gender, :string
+    field :email, :string
+    field :tax_id, :string
+    field :national_id, :string
+    field :death_date, :date
+    field :is_active, :boolean
+    field :documents, :map
+    field :addresses, :map
+    field :phones, :map
+    field :history, :map
+    field :inserted_by, :string
+    field :updated_by, :string
+
+    timestamps(type: :utc_datetime)
+  end
+
+  @fields ~W(
+    first_name
+    last_name
+    second_name
+    birth_date
+    gender
+    email
+    tax_id
+    national_id
+    death_date
+    is_active
+    documents
+    addresses
+    phones
+    history
+    inserted_by
+    updated_by
+  )
+
+  def changeset(struct, params \\ %{}) do
+    struct
+    |> cast(params, @fields)
+    |> validate_required(:first_name)
+    |> validate_required(:last_name)
+    |> validate_required(:birth_date)
+    |> validate_required(:gender)
+    |> validate_required(:inserted_by)
+    |> validate_required(:updated_by)
+  end
+end
