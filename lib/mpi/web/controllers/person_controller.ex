5ef3c4265fbef37830215a1c38cc951423d9de5f
samorai <oleg.samorai@gmail.com>
2017-03-13 17:22:05 +0200

    Add MPI Record API

--- lib/mpi/web/controllers/person_controller.ex
+++ lib/mpi/web/controllers/person_controller.ex
@@ -2,12 +2,21 @@ defmodule Mpi.Web.PersonController do
   @moduledoc false
   use Mpi.Web, :controller
   alias Mpi.Repo
+  alias Mpi.Person
   action_fallback Mpi.Web.FallbackController
 
   def get_person(conn, %{"id" => id}) do
     with %Mpi.Person{} = person <- Repo.get(Mpi.Person, id) do
       conn
       |> put_status(:ok)
+      |> render("person.json", %{person: person})
+    end
+  end
+
+  def create_person(conn, params) do
+    with {:ok, person} <- Repo.insert(Person.changeset(%Person{}, params)) do
+      conn
+      |> put_status(:created)
       |> render("person.json", %{person: person})
     end
   end
