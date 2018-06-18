66c4d8be92a1481034334c06a9e5ccb9b46a61af
Maryna <marinakrasnovatr81@gmail.com>
2018-06-18 15:20:29 +0300

    [minor] Write documents and phones into related tables

--- priv/repo/migrations/20180618111027_add_inserted_at_index.exs
+++ priv/repo/migrations/20180618111027_add_inserted_at_index.exs
@@ -2,12 +2,11 @@ defmodule MPI.Repo.Migrations.AddInsertedAtIndex do
   use Ecto.Migration
   @disable_ddl_transaction true
 
-  @schemas [:person_documents, :person_phones, :persons]
   def up do
-    Enum.each(@schemas, fn shemaname -> create(index(shemaname, [:inserted_at], concurrently: true)) end)
+    create(index(:persons, [:inserted_at], concurrently: true))
   end
 
   def down do
-    Enum.each(@schemas, fn shemaname -> drop(index(shemaname, [:inserted_at], concurrently: true)) end)
+    drop(index(:persons, [:inserted_at], concurrently: true))
   end
 end
