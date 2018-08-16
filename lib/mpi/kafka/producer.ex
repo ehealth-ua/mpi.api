ed82a937c26724ead54fd144bae6a4822a5ef0c9
Alex Kovalevych <AlexKovalevych@users.noreply.github.com>
2018-08-16 17:00:52 +0300

    create person events migration (#115)

--- lib/mpi/kafka/producer.ex
+++ lib/mpi/kafka/producer.ex
@@ -7,7 +7,7 @@ defmodule MPI.Kafka.Producer do
   require Logger
 
   def publish_person_event(id, status) do
-    event = %{"id" => id, "status" => status}
+    event = %{"id" => id, "status" => String.downcase(status)}
 
     with :ok <- KafkaEx.produce(@person_events_topic, 0, :erlang.term_to_binary(event)) do
       Logger.info("Published event #{inspect(event)} to kafka", application: :kafka_ex)
---
+++ priv/repo/migrations/20180816132038_publish_person_events.exs
@@ -0,0 +1,33 @@
+defmodule MPI.Repo.Migrations.PublishPersonEvents do
+  use Ecto.Migration
+
+  alias MPI.Kafka.Producer
+  alias MPI.Person
+  alias MPI.Repo
+  import Ecto.Query
+
+  @disable_ddl_transaction true
+
+  def change do
+    publish_events(0)
+  end
+
+  defp publish_events(offset) do
+    persons =
+      Person
+      |> select([p], [:id, :status])
+      |> offset(^offset)
+      |> limit(500)
+      |> Repo.all()
+
+    Enum.map(persons, fn person ->
+      Producer.publish_person_event(person.id, person.status)
+    end)
+
+    if Enum.empty?(persons) do
+      :ok
+    else
+      publish_events(offset + 500)
+    end
+  end
+end
