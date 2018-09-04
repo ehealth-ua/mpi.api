2209e7cb55ac1caf9b11d5632860d8fe7aadf895
Alex Kovalevych <AlexKovalevych@users.noreply.github.com>
2018-09-04 14:01:44 +0300

    fix start container (#123)

--- apps/mpi/config/config.exs
+++ apps/mpi/config/config.exs
@@ -5,3 +5,5 @@ config :mpi, MPI.Web.Endpoint,
   url: [host: "localhost"],
   secret_key_base: "bvzeKHzH8k+qavTDh5NTxFcnPVHIL+Ybi1Bucq2TrJ3I3zqbXEqFr37QbrL0c202",
   render_errors: [view: EView.Views.PhoenixError, accepts: ~w(json)]
+
+import_config "#{Mix.env()}.exs"
