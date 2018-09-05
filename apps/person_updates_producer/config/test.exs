0b0a96ef38daf4e6478a5355ad81940327a86dd4
Alex Kovalevych <AlexKovalevych@users.noreply.github.com>
2018-09-05 10:41:24 +0300

    stop process when job is done (#127)

--- apps/person_updates_producer/config/test.exs
+++ apps/person_updates_producer/config/test.exs
@@ -3,7 +3,8 @@ use Mix.Config
 config :person_updates_producer,
   kafka: [
     producer: KafkaMock
-  ]
+  ],
+  worker: WorkerMock
 
 # Print only warnings and errors during test
 config :logger, level: :warn
