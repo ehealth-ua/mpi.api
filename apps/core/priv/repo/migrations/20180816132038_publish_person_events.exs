88b8d5d14b15655bf669d353fbd466dcc637fef6
Alex Kovalevych <AlexKovalevych@users.noreply.github.com>
2018-09-21 18:18:54 +0300

    fix initial person updates migration (#135)

--- apps/core/priv/repo/migrations/20180816132038_publish_person_events.exs
+++ apps/core/priv/repo/migrations/20180816132038_publish_person_events.exs
@@ -1,28 +1,34 @@
 defmodule Core.Repo.Migrations.PublishPersonEvents do
+  @moduledoc false
+
   use Ecto.Migration
 
-  alias Core.Kafka.Producer
   alias Core.Person
+  alias Core.PersonUpdate
   alias Core.Repo
   import Ecto.Query
 
   @disable_ddl_transaction true
 
   def change do
-    Application.ensure_all_started(:kafka_ex)
     publish_events(0)
   end
 
   defp publish_events(offset) do
     persons =
       Person
-      |> select([p], [:id, :status])
+      |> select([p], [:id, :status, :updated_by, :updated_at])
       |> offset(^offset)
       |> limit(500)
       |> Repo.all()
 
-    Enum.map(persons, fn person ->
-      Producer.publish_person_event(person.id, person.status, person.updated_by)
+    Enum.each(persons, fn person ->
+      Repo.insert(%PersonUpdate{
+        person_id: person.id,
+        updated_by: person.updated_by,
+        updated_at: person.updated_at,
+        status: person.status
+      })
     end)
 
     if Enum.empty?(persons) do
