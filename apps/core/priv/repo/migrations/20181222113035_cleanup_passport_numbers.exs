8dc1e8f263263e98e29e038f3ad6df0970c7fd3b
Maxim Batrak <43367461+BatrakM@users.noreply.github.com>
2018-12-26 13:18:22 +0200

    Trim spaces and update to uppercase person docs with type = 'PASSPORT' (#163)

---
+++ apps/core/priv/repo/migrations/20181222113035_cleanup_passport_numbers.exs
@@ -0,0 +1,35 @@
+defmodule Core.Repo.Migrations.CleanupPassportNumbers do
+  use Ecto.Migration
+
+  use Ecto.Migration
+  import Ecto.Query
+  alias Core.Repo
+  alias Core.PersonDocument
+
+  def change, do: chunk_update(10000)
+
+  defp cleanup_number(limit, offset) do
+    from(pd in PersonDocument,
+      where:
+        fragment(
+          "id in (select id from person_documents where type = 'PASSPORT' order by inserted_at offset ? limit ?)",
+          ^offset,
+          ^limit
+        ),
+      update: [
+        set: [
+          number:
+            fragment("upper(array_to_string(regexp_split_to_array(TRIM(number), ' {2,}'), ' '))")
+        ]
+      ]
+    )
+    |> Repo.update_all([])
+  end
+
+  defp chunk_update(limit, offset \\ 0) do
+    case cleanup_number(limit, offset) do
+      {0, nil} -> :ok
+      {count, nil} -> chunk_update(limit, offset + count)
+    end
+  end
+end
