884b1bf024f45b150586a1d2caeabf592ce4a234
Maxim Batrak <43367461+BatrakM@users.noreply.github.com>
2019-02-11 16:37:48 +0200

    fix: cleanup_passport_numbers migration now continues work after failure (#216)

--- apps/core/priv/repo/migrations/20181222113035_cleanup_passport_numbers.exs
+++ apps/core/priv/repo/migrations/20181222113035_cleanup_passport_numbers.exs
@@ -4,14 +4,40 @@ defmodule Core.Repo.Migrations.CleanupPassportNumbers do
   alias Core.Repo
   alias Core.PersonDocument
 
-  def change, do: chunk_update(10000)
+  @disable_ddl_transaction true
 
-  defp cleanup_number(limit, offset) do
+  def change do
+    create_if_not_exists table(:cleanup_passport_numbers_temp) do
+      add(:last_inserted_at, :naive_datetime)
+    end
+
+    flush()
+
+    with :ok <- chunk_update(500, get_last_inserted_at()) do
+      drop table(:cleanup_passport_numbers_temp)
+    end
+  end
+
+  defp get_last_inserted_at do
+    query = from(t in "cleanup_passport_numbers_temp", select: t.last_inserted_at)
+
+    case Repo.all(query) do
+      [last_inserted_at] ->
+        last_inserted_at
+
+      [] ->
+        last_inserted_at = ~N[1970-01-01 00:00:00.000]
+        Repo.insert_all("cleanup_passport_numbers_temp", [[last_inserted_at: last_inserted_at]])
+        last_inserted_at
+    end
+  end
+
+  defp cleanup_number(limit, last_inserted_at) do
     from(pd in PersonDocument,
       where:
         fragment(
-          "id in (select id from person_documents where type = 'PASSPORT' order by inserted_at offset ? limit ?)",
-          ^offset,
+          "id in (select id from person_documents where type = 'PASSPORT' and inserted_at >= ? order by inserted_at limit ?)",
+          ^last_inserted_at,
           ^limit
         ),
       update: [
@@ -21,13 +47,23 @@ defmodule Core.Repo.Migrations.CleanupPassportNumbers do
         ]
       ]
     )
-    |> Repo.update_all([])
+    |> Repo.update_all([], returning: [:inserted_at])
   end
 
-  defp chunk_update(limit, offset \\ 0) do
-    case cleanup_number(limit, offset) do
-      {0, nil} -> :ok
-      {count, nil} -> chunk_update(limit, offset + count)
+  defp chunk_update(limit, last_inserted_at) do
+    case cleanup_number(limit, last_inserted_at) do
+      {0, []} ->
+        :ok
+
+      {_, updates} ->
+        last_inserted_at =
+          updates
+          |> List.last()
+          |> Map.get(:inserted_at)
+
+        Repo.update_all("cleanup_passport_numbers_temp", set: [last_inserted_at: last_inserted_at])
+
+        chunk_update(limit, last_inserted_at)
     end
   end
 end
