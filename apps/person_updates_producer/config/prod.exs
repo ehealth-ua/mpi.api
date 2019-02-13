b17e66bdca8247ad5bf266f13141ee4dfdf9512c
Pavel Vesnin <pavel.vesnin.v@gmail.com>
2019-02-13 15:46:11 +0200

    chore: revert Kafka brokers config (#225)

--- apps/person_updates_producer/config/prod.exs
+++ apps/person_updates_producer/config/prod.exs
@@ -1,6 +1,6 @@
 use Mix.Config
 
-config :kafka_ex, brokers: "$KAFKA_BROKERS"
+config :kafka_ex, brokers: "${KAFKA_BROKERS}"
 
 # Do not print debug messages in production
 config :logger, level: :info
