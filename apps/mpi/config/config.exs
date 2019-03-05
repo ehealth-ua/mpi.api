0af9a62e91c1c891302df47aec7ec12042adaf6c
Alex Kovalevych <AlexKovalevych@users.noreply.github.com>
2019-03-05 14:59:49 +0200

    fix: phoenix errors logging (#262)

--- apps/mpi/config/config.exs
+++ apps/mpi/config/config.exs
@@ -4,7 +4,8 @@ use Mix.Config
 config :mpi, MPI.Web.Endpoint,
   url: [host: "localhost"],
   secret_key_base: "bvzeKHzH8k+qavTDh5NTxFcnPVHIL+Ybi1Bucq2TrJ3I3zqbXEqFr37QbrL0c202",
-  render_errors: [view: EView.Views.PhoenixError, accepts: ~w(json)]
+  render_errors: [view: EView.Views.PhoenixError, accepts: ~w(json)],
+  instrumenters: [LoggerJSON.Phoenix.Instruments]
 
 config :kaffe,
   producer: [
--- apps/mpi/config/dev.exs
+++ apps/mpi/config/dev.exs
@@ -3,7 +3,7 @@ use Mix.Config
 config :mpi, MPI.Web.Endpoint,
   load_from_system_env: true,
   http: [port: {:system, "APP_PORT", 4000}],
-  debug_errors: true,
+  debug_errors: false,
   code_reloader: true,
   check_origin: false,
   watchers: []
