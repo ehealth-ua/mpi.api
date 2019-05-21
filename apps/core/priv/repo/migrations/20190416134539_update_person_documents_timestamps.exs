af66003082335c4226df726ff385c960950b8538
Denis Klimenko <denis.klimenko.dx@gmail.com>
2019-05-21 11:07:24 +0300

    fix: inserted at in person documents, attempt 2 (#324)

--- apps/core/priv/repo/migrations/20190416134539_update_person_documents_timestamps.exs
+++ apps/core/priv/repo/migrations/20190416134539_update_person_documents_timestamps.exs
@@ -1,28 +1,33 @@
 defmodule Core.Repo.Migrations.UpdatePersonDocumentsTimestamps do
   use Ecto.Migration
 
+  import Ecto.Query
+
   alias Core.Repo
   alias Ecto.Adapters.SQL
   alias Ecto.UUID
 
-  import Ecto.Query
-
   @disable_ddl_transaction true
-  @batch_size 100
+  @batch_size 500
 
   def change do
     execute("CREATE TABLE IF NOT EXISTS person_documents_update_temp AS SELECT id, inserted_at FROM persons")
+    execute("CREATE INDEX IF NOT EXISTS person_documents_update_temp_index ON person_documents_update_temp(id)")
+
+    execute("CREATE TABLE IF NOT EXISTS person_documents_update_temp2 AS SELECT id, inserted_at FROM person_documents_update_temp ORDER BY id ASC")
     flush()
 
-    chunk_update()
+    batch_update()
 
-    execute("DROP TABLE person_documents_update_temp")
+    execute("DROP TABLE IF EXISTS person_documents_update_temp")
+    execute("DROP TABLE IF EXISTS person_documents_update_temp2")
   end
 
-  defp chunk_update do
-    from(pd in "person_documents_update_temp",
+  defp batch_update do
+    from(pd in "person_documents_update_temp2",
       select: {pd.id, pd.inserted_at},
-      limit: @batch_size
+      limit: @batch_size,
+      order_by: :id
     )
     |> Repo.all()
     |> do_update()
@@ -31,37 +36,33 @@ defmodule Core.Repo.Migrations.UpdatePersonDocumentsTimestamps do
   defp do_update([]), do: :ok
 
   defp do_update(rows) do
-    {ids, update_values} =
-      Enum.reduce(rows, {[], []}, fn {id, inserted_at}, {ids, patch} ->
-        {
-          [id | ids],
-          [
-            "('#{UUID.cast!(id)}'::uuid, '#{inserted_at}'::timestamp)"
-            | patch
-          ]
-        }
-      end)
+    {last_id, _} = List.last(rows)
 
-    case update_person_documents(Enum.join(update_values, ", ")) do
+    update_values =
+      rows
+      |> Enum.map(fn {id, inserted_at} -> "('#{UUID.cast!(id)}'::uuid, '#{inserted_at}'::timestamp)" end)
+      |> Enum.join(", ")
+
+    case update_person_documents(update_values) do
       {:ok, _} ->
-        delete_temp_records(ids)
-        chunk_update()
+        drop_temp_records(last_id)
+        batch_update()
 
-      err ->
-        raise "Fail to migrate person documents timestamps. Error: #{inspect(err)}"
+      error ->
+        raise "Fail to migrate person documents timestamps. Error: #{inspect(error)}"
     end
   end
 
   defp update_person_documents(values) do
     SQL.query(Repo, """
-      UPDATE person_documents 
+      UPDATE person_documents
       SET inserted_at = tmp.inserted_at
       FROM (VALUES #{values}) AS tmp (id, inserted_at)
       WHERE tmp.id = person_documents.person_id;
     """)
   end
 
-  defp delete_temp_records(ids) do
-    from(pd in "person_documents_update_temp", where: pd.id in ^ids) |> Repo.delete_all()
+  defp drop_temp_records(last_id) do
+    SQL.query(Repo, "DELETE FROM person_documents_update_temp2 WHERE id <= '#{UUID.cast!(last_id)}'::uuid")
   end
 end
