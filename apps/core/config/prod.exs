d498bf356a2c4f09c4df2d1ddbab26857cc50413
Denis Klimenko <denis.klimenko.dx@gmail.com>
2019-06-03 17:08:50 +0300

    fix: set queue target and interval for ecto repos (#331)

--- apps/core/config/prod.exs
+++ apps/core/config/prod.exs
@@ -1,5 +1,9 @@
 use Mix.Config
 
+# 1 week in ms
+queue_target = 604_800_000
+queue_interval = queue_target + 1000
+
 config :core, Core.Repo,
   username: {:system, :string, "DB_USER"},
   password: {:system, :string, "DB_PASSWORD"},
@@ -7,7 +11,9 @@ config :core, Core.Repo,
   hostname: {:system, :string, "DB_HOST"},
   port: {:system, :integer, "DB_PORT"},
   pool_size: {:system, :integer, "POOL_SIZE", 10},
-  timeout: :infinity
+  timeout: :infinity,
+  queue_target: queue_target,
+  queue_interval: queue_interval
 
 config :core, Core.ReadRepo,
   username: {:system, :string, "READ_DB_USER"},
@@ -16,7 +22,9 @@ config :core, Core.ReadRepo,
   hostname: {:system, :string, "READ_DB_HOST"},
   port: {:system, :integer, "READ_DB_PORT"},
   pool_size: {:system, :integer, "READ_DB_POOL_SIZE", 10},
-  timeout: :infinity
+  timeout: :infinity,
+  queue_target: queue_target,
+  queue_interval: queue_interval
 
 config :core, Core.DeduplicationRepo,
   username: {:system, :string, "DB_DEDUPLICATION_USER"},
@@ -25,4 +33,6 @@ config :core, Core.DeduplicationRepo,
   hostname: {:system, :string, "DB_DEDUPLICATION_HOST"},
   port: {:system, :integer, "DB_DEDUPLICATION_PORT"},
   pool_size: {:system, :integer, "DEDUPLICATION_POOL_SIZE", 10},
-  timeout: :infinity
+  timeout: :infinity,
+  queue_target: queue_target,
+  queue_interval: queue_interval
